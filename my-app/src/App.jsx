6c0112a4b0c65ea7cab5b42ea7c523c62dd372a0
sarthaksaitwal <sarthaksaitwal3@gmail.com>
2025-09-02 13:46:51 +0530

    New

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <main className="content">
+        <h2>Sidebar Preview</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
